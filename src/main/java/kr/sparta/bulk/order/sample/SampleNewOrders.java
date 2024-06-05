2732b744fd7477349b22ef258d5b601eb010fb7b
Suncheol Kim <suncheol.kim84@gmail.com>
2024-06-05 11:22:36 +0900

    1단계] feat : 주문 대량 생성, 수정 기능

---
+++ src/main/java/kr/sparta/bulk/order/sample/SampleNewOrders.java
@@ -0,0 +1,97 @@
+package kr.sparta.bulk.order.sample;
+
+import kr.sparta.bulk.order.model.OrderCreateRequest;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SampleNewOrders {
+    public static List<OrderCreateRequest> get() {
+        List<OrderCreateRequest> orders = new ArrayList<>(150);
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이아름").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이유진").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("구본철").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("권진석").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("박지영").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("강준혁").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("양제훈").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("임현수").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김민주").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("백은영").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("허수연").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김동건").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("박시환").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김태훈").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("장은영").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("민희은").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("정원휘").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("임재현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김도균").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("유영상").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("정대현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("동우정").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("장대산").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김민선").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("송정학").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("안정민").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("유승희").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("박재혁").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("허준").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이창현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김단빈").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("손민석").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김지윤").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("천경환").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이경섭").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("장우태").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("장수연").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김현준").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("진하빈").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("고강혁").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김민상").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이예지").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("주은수").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이빛나").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("정가현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김지연").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김태현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("하진이").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("정선우").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("안재솔").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("조현지").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김휘웅").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("한지연").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김명호").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김영웅").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이한준").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("양혁진").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("류병길").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("박예진").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이승현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("위승현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김민").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김민경").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("박재홍").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("편효준").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("조성민").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("천준민").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이해욱").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("전수연").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("권재현").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("박무근").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("이서준").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김영준").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("정건우").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("황상익").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("한승완").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("박용재").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김세원").customerAddress("대한민국").build());
+        orders.add(OrderCreateRequest.builder().product("민초").quantity(2).customerName("김민범").customerAddress("대한민국").build());
+        return orders;
+    }
+}
+
+
